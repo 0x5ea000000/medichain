24c16ce4a15e9dc6652edd8bffbfb2bf5789b61f
Vu <Vu>
2021-10-05 22:46:15 +0700

    Merged PR 2035: feat: fix status = accepted
    feat: fix status = accepted

--- x/medichain/keeper/msg_server_create_sharing_batch.go
+++ x/medichain/keeper/msg_server_create_sharing_batch.go
@@ -9,7 +9,7 @@ import (
 func (k msgServer) CreateSharingBatch(goCtx context.Context, msg *types.MsgCreateSharingBatch) (*types.MsgCreateSharingBatchResponse, error) {
 	// TODO: Handling the message
 	for _, v := range msg.OwnerIds {
-		m := types.MsgCreateSharing{Creator: msg.Creator, ViewerId: msg.ViewerId, OwnerId: v}
+		m := types.MsgCreateSharing{Creator: msg.Creator, ViewerId: msg.ViewerId, OwnerId: v, Status: types.ACCEPTED}
 		_, err := k.CreateSharing(goCtx, &m)
 		if err != nil {
 			return nil, err
