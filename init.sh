7c9deda7f4eef14337e8838f15b340379e116f86
Vu Nguyen <54926438+aquarius6666@users.noreply.github.com>
2021-11-12 14:13:27 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 rm -Rf ~/.medichain && medichaind init $MONIKER --chain-id $CHAIN_ID --overwrite 
 echo $VALIDATOR_MNEMONIC | medichaind keys add $VALIDATOR_ACCOUNT --hd-path "m/44'/118'/0'/0" --recover --interactive=false
 medichaind add-genesis-account $VALIDATOR_ACCOUNT $AMOUNT
